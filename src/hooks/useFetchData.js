d9bb56f39d1ca7aee6e186f8cf42395b80776e66
IvanMedentsi <medentsi.ivan@student.uzhnu.edu.ua>
2024-11-03 21:48:09 +0200

    lab_6/7

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,33 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
+import { database } from '../firebase'; // Імпортуйте вашу конфігурацію
+
+const useFetchData = (collectionName) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(database, collectionName));
+                const fetchedData = querySnapshot.docs.map(documentSnapshot => ({
+                    id: documentSnapshot.id,
+                    ...documentSnapshot.data()
+                }));
+                setData(fetchedData);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [collectionName]);
+
+    return { data, loading, error };
+};
+
+export default useFetchData;
